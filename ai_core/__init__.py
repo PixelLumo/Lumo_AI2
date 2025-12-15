60b329aaa346a43a2587f4f91295cdc9020a4b45
Lumo <taineh42@gmail.com>
2025-12-15 16:48:57 +0200

    Delete ai_core directory

--- ai_core/__init__.py
+++
@@ -1,3 +0,0 @@
-"""ai_core package exports."""
-
-from .ai_core import handle_query
--- ai_core/voice_speaker.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-import config
-
-engine = pyttsx3.init()
-engine.setProperty('voice', config.SETTINGS['voice'])
-
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
